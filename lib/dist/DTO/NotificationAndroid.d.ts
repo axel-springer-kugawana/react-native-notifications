8d6e240e28a30718ab583f7efd9425ba2dcd1c4e
Dudi Nigrad <dudin@yad2.co.il>
2020-01-22 14:58:20 +0200

    android notifications with group

---
+++ lib/dist/DTO/NotificationAndroid.d.ts
@@ -0,0 +1,8 @@
+import { Notification } from './Notification';
+export declare class NotificationAndroid extends Notification {
+    constructor(payload: object);
+    readonly title: string;
+    readonly body: string;
+    readonly sound: string;
+    readonly group: string;
+}
